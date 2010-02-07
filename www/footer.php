178c6899bdcdbc4764a07f9cb227fc12b26fd15c
fguilhaumon <fguilhaumon@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-07 13:17:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mmsar@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/footer.php
+++ www/footer.php
@@ -25,7 +25,25 @@
 	<div class="center-wrapper">
 
 		<div class="left">
-			The web site is &copy; 2009 <a href="http://www.ecolag.univ-montp2.fr/fguilhaumon" target="_new">François Guilhaumon</a> - mmSAR is <a href="http://www.gnu.org/licenses/gpl.html" target="_new">GNU/GPL</a> feel free to use it and <a href="http://r-forge.r-project.org/projects/mmsar/" target="_new">contribute</a> to it ! 
+			The web site is &copy; 2009 <a href="http://www.ecolag.univ-montp2.fr/fguilhaumon" target="_new">François Guilhaumon</a> - mmSAR is <a href="http://www.gnu.org/licenses/gpl.html" target="_new">GNU/GPL</a> feel free to use it and <a href="http://r-forge.r-project.org/projects/mmsar/" target="_new">contribute</a> to it ! <a href="http://www.xiti.com/xiti.asp?s=446326" title="WebAnalytics" target="_top">
+<script type="text/javascript">
+<!--
+Xt_param = 's=446326&p=';
+try {Xt_r = top.document.referrer;}
+catch(e) {Xt_r = document.referrer; }
+Xt_h = new Date();
+Xt_i = '<img width="80" height="15" border="0" alt="" ';
+Xt_i += 'src="http://logv11.xiti.com/g.xiti?'+Xt_param;
+Xt_i += '&hl='+Xt_h.getHours()+'x'+Xt_h.getMinutes()+'x'+Xt_h.getSeconds();
+if(parseFloat(navigator.appVersion)>=4)
+{Xt_s=screen;Xt_i+='&r='+Xt_s.width+'x'+Xt_s.height+'x'+Xt_s.pixelDepth+'x'+Xt_s.colorDepth;}
+document.write(Xt_i+'&ref='+Xt_r.replace(/[<>"]/g, '').replace(/&/g, '$')+'" title="Internet Audience">');
+//-->
+</script>
+<noscript>
+Mesure d'audience ROI statistique webanalytics par <img width="80" height="15" src="http://logv11.xiti.com/g.xiti?s=446326&p=" alt="WebAnalytics" />
+</noscript></a>
+
 		</div>
 
 		<div class="right">
